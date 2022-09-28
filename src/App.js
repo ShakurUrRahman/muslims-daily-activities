f0f6d81c30a09f1cba9bec83d6364f1883042cf1
ShakurUrRahman <you@example.comshakururrahman@gmail.com>
2022-09-28 22:10:05 +0600

    activity part done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Activity from './components/Activity/Activity';
 
 function App() {
   return (
     <div className="App">
-      <h1>Muslims Daily Activities</h1>
+      <LoadActivities></LoadActivities>
     </div>
   );
+
+  function LoadActivities() {
+    const [activities, setActivities] = useState([]);
+    const [time, setTime] = useState([])
+
+    useEffect(() => {
+      fetch('activities.json')
+        .then(res => res.json())
+        .then(data => setActivities(data))
+    }, [])
+
+
+
+
+    return (
+      <div className='grid lg:grid-cols-4'>
+        <div className='col-span-3 ml-28 my-20'>
+          <h1 className='text-3xl font-bold text-green-800 mb-3'>Muslims Daily Activities</h1>
+          <h1 className='text-2xl font-bold text-green-500 mb-5'>Select your activities</h1>
+          <div className='grid lg:grid-cols-3 gap-2'>
+            {
+              activities.map(activity => <Activity
+                key={activity.id}
+                activity={activity}
+              ></Activity>)
+            }
+          </div>
+        </div>
+        <div className='col-span-1'>
+          <h1>Details</h1>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Activity/Activity.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Activity = (props) => {
+    const { picture, name, time } = props.activity;
+    return (
+        <div className="card w-64 bg-base-100 shadow-xl">
+            <figure className="px-5 pt-5">
+                <img src={picture} alt="Shoes" className="rounded-xl h-52 w-96" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{name}</h2>
+                <p>{time} minutes</p>
+                <div className="card-actions">
+                    <button className="btn btn-primary">Add to list</button>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
+export default Activity;
